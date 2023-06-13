100a5c4eeac4aa0c7eb291ea489e85e4de2cf5e8
72464_Mayuri <mayuripatade08@gmail.com>
2023-06-13 11:32:10 +0530

    inline

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 function App() {
-  let id = "id1";
-  let str = `Hello World`;
-  let pi = 3.14;
 
- 
+  let style = { background: "green",color: "white"};
   return (
-   <>
-   <h1 id="id1">Hello World</h1>
-   <h1 id={id}>{str}</h1>
+    <>
+    <h1 style={style}>Hello World</h1>
 
-   <h1>PI {pi} !!</h1>
-  
-   <h1 id="id1">Hello World</h1>
-   <h1 id={"id1"}>Hello World</h1>
-   <h1 id={`id1`}>Hello World</h1>
-   <h1 id={id}>Hello World</h1>
-   </>
+    <h1 style = {{ background: "blue",color: "white"}}>Hello World</h1>
+
+    <h1 style = {{ backgroundColor: "red",color:"white",padding: "8px"}}>Hello World</h1>
+    </>
   );
- }
- 
- export default App;
+}
+
+export default App;
