696a52c218dd5f00e1ae46c75f57a0677447ee2b
72464_Mayuri <mayuripatade08@gmail.com>
2023-06-13 11:42:47 +0530

    external

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import "./App.css";
+
 function App() {
 
-  let style = { background: "green",color: "white"};
+ 
   return (
     <>
-    <h1 style={style}>Hello World</h1>
+    <h1 className="beautiful">Hello World</h1>
 
-    <h1 style = {{ background: "blue",color: "white"}}>Hello World</h1>
-
-    <h1 style = {{ backgroundColor: "red",color:"white",padding: "8px"}}>Hello World</h1>
     </>
   );
 }
