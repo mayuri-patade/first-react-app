4f874ac4ea1edbfac1a6ee62490c496680e47e2f
72464_Mayuri <mayuripatade08@gmail.com>
2023-06-13 11:59:17 +0530

    dum website

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
  
   return (
     <>
-    <h1 >Hello World</h1>
+    <h1 className="bg-primary text-light p-3">My Project</h1>
+
+    <div className="alert alert-primary">
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h1>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, omnis numquam similique sequi nihil repellendus enim recusandae suscipit impedit dolor beatae placeat minima, quae molestiae tempora exercitationem laborum. Amet, quasi?</div>
 
     </>
   );
