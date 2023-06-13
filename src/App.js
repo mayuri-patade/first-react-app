4238fc96217a7dd79940b4af3fec57dad011c538
72464_Mayuri <mayuripatade08@gmail.com>
2023-06-13 09:34:40 +0530

    syntax

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
+//JSX : REturn HTML
+//Syntax Rule :: If one linear parenthsis are optional
+//multi linear Parenthetsis are required
+//Syntax :: () //Empty JSX
+//Syntax :: (<h1>Hello</h1>)
 function App() {
+  return (
   <>
-  return <h1>Hello</h1>;
+  <h1>Hello World</h1>
   </>
+  );
 }
 
 export default App;
