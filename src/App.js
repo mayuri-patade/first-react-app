1c6485035b7fcce2c0f7d4aedee522c0adf8a438
72464_Mayuri <mayuripatade08@gmail.com>
2023-06-13 10:33:05 +0530

    object interpolation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-//JSX :: View
 function App() {
  let str = `Hello World`;
  let pi = 3.14;
  let active = true;
  let isUserLogIn = true;
+ let user = {id : 1,name : "mayuri"};
 
  return (
   <>
@@ -12,16 +12,8 @@ function App() {
   <h1>{str.toUpperCase()}</h1>
   <h1>{str.substring(1)}</h1>
   <h1>PI {pi} !!</h1>
+  <h1>ID:{user.id} NAME:{user.name}</h1>
 
-  {/**Login */}
-  {isUserLogIn && <h1>Welcome To React</h1>}
-  
-  {/** Boolean usage */}
-  <h1>{active ? "morning" : "night"}</h1>
-
-  {/**Most useful */}
-  {active && <h1>Morning</h1>}
-  {active ? <h1>Morning</h1> : <h1>Night</h1>}
   </>
  );
 }
