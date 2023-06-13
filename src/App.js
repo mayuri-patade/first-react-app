f4e21ea05deb4b2d75f22d9d09181e696ee08a52
72464_Mayuri <mayuripatade08@gmail.com>
2023-06-13 11:12:23 +0530

    attribute binding

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-// JSX = JS + HTML
-// IF A FUNCTION RETURNS HTML, THIS BECOMES A USER DEFINED TAG
-// <App></App>
-// <App />
-// Compare with PreDefineTag
-// h1, p, li,
-// Why Master Case Convention ?
-// For Pre Define Tag  :: Lowercase :: <h1>
-// For User Define Tag :: MasterCase :: <App>
 function App() {
-  return <h1>Hello</h1>;
-}
+  let id = "id1";
+  let str = `Hello World`;
+  let pi = 3.14;
 
-// UNUSED!!!
-// JSX => JS + HTML/XML
-// IF your function returns HTML
-// Convention:: Start your function name in captial letter.
-function Sum() {
-  return <h1>100</h1>;
-}
+ 
+  return (
+   <>
+   <h1 id="id1">Hello World</h1>
+   <h1 id={id}>{str}</h1>
 
-export default App;
+   <h1>PI {pi} !!</h1>
+  
+   <h1 id="id1">Hello World</h1>
+   <h1 id={"id1"}>Hello World</h1>
+   <h1 id={`id1`}>Hello World</h1>
+   <h1 id={id}>Hello World</h1>
+   </>
+  );
+ }
+ 
+ export default App;
